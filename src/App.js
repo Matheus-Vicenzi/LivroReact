a9899cbd7eff20a39c6feb8b4ce82363a25082ca
matheus.vicenzi <matheus.vicenzi@neomind.com.br>
2023-03-10 14:25:14 -0300

    Capitulo 3

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import TabelaBody from "./components/TabelaBody";
+import TabelaFoot from "./components/TabelaFoot";
+import TabelaHead from "./components/TabelaHead";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    livros: [{
+      id: "978-85-7522-632-2",
+      titulo: "CSS Grid Layout",
+      autor: "Maurício Samy Silva"
+    },
+    {
+      id: "978-85-7522-677-3",
+      titulo: "Node Essencial",
+      autor: "Ricardo R. Lecheta"
+    },
+    {
+      id: "978-85-7522-522-7",
+      titulo: "Aprendendo Material Design",
+      autor: "Kyle Mew"
+    }
+    ]
+  };
+
+  render() {
+    return (
+      <table className="tabela">
+        <TabelaHead />
+        <TabelaBody livros = {this.state.livros} />
+        <TabelaFoot qdeLivros = {this.state.livros.length}/>
+      </table>
+    );
+  }
 }
-
 export default App;
